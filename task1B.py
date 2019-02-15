48810316b9326c2d83db3bcbc476e0796fa8b607
GurleenKaur828 <gurleen.kaur@dal.ca>
2019-02-15 11:07:22 -0800

    task 1B

---
+++ task1B.py
@@ -0,0 +1,15 @@
+filename=open("Book3.txt")
+def process_file(filename):
+	hist = dict()
+	for line in filename:
+		most_common(hist)
+	return hist
+def most_common(hist):
+	t = []
+	for key, value in hist.items():
+		t.append((value, key))
+		t.sort(reverse=True)
+	return t
+
+hist = process_file('Book3.txt')
+
